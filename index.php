98acae4c4c22f84995e2ac38a62bf0dace050cf8
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-04-24 13:23:48 +1200

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,12 @@
 <?php
+function Redirect($url, $permanent = false)
+{
+    header('Location: ' . $url, true, $permanent ? 301 : 302);
+
+    exit();
+}
+?>
+<?php
+Redirect('index.php', false);
 echo "hello sir1"
 ?>
