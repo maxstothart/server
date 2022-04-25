331eab8cb7bf29a9c82d1d2cb86f7e840a0e60a1
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-04-25 16:02:10 +1200

    cron

--- dev.php
+++ dev.php
@@ -1,3 +1,20 @@
-<?php
-system('echo hello')
-?>
+<!DOCTYPE html>
+<?php if($_SESSION["username"] == "admin"){ ?>
+    <html>
+        <head>
+            <title>Admin Console</title>
+        </head>
+        <body>
+            <h1>admin</h1>
+</body>
+    </html>
+<?php }else{ ?>
+    <html>
+        <head>
+            <title>Webpage</title>
+        </head>
+        <body>
+            <h1>user</h1>
+        </body>
+    </html>
+<?php } ?>
