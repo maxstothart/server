f9cae134ce362386a71d38ef0f557cae37c3185b
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-04-25 16:15:14 +1200

    dev

--- dev.php
+++ dev.php
@@ -11,22 +11,39 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
 
 <!DOCTYPE html>
 <?php if($_SESSION["username"] == "admin"){ ?>
-    <html>
-        <head>
-            <title>Admin Console</title>
-        </head>
-        <body>
-            <h1>admin</h1>
-            <p>
+    <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin Console</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <h1 class="my-5">Hi, <b>Admin</b>. What do you want to do</h1>
+    <p>
+        <a href="#">Reboot server</a>
+        <a href="logout.php" class="btn btn-danger ml-3">Sign Out</a>
+    </p>
 </body>
-    </html>
+</html>
 <?php }else{ ?>
-    <html>
-        <head>
-            <title>Webpage</title>
-        </head>
-        <body>
-            <h1>user</h1>>
-        </body>
-    </html>
+    <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Welcome</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to Windmill INC</h1>
+    <p>
+        <a href="reset-password.php" class="btn btn-warning">Reset Your Password</a>
+        <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+    </p>
+</body>
+</html>
 <?php } ?>
