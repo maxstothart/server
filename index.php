382641b026ef4b661e8c010c1e9370b33f10529e
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-04-26 20:00:51 +1200

    replace index.php with dev.php and deleted it

--- index.php
+++ index.php
@@ -8,13 +8,53 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     exit;
 }
 ?>
- 
+
 <!DOCTYPE html>
-<html lang="en">
+<?php if($_SESSION["username"] == "admin"){ 
+if (isset($_GET['reboot'])) {
+    exec("sudo reboot");
+    header('Location: index.php');
+} elseif (isset($_GET['restart'])) {
+    exec("sudo systemctl restart apache2.service");
+    header('Location: index.php');
+} elseif (isset($_GET['update'])) {
+    exec("updateserver");
+    header('Location: index.php');
+}?>
+    <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin Console</title>
+    <link rel="stylesheet" href="style.css">
+    <style>
+        body{ font: 14px sans-serif; text-align: center; }
+    </style>
+</head>
+<body>
+    <h1 class="my-5">Hi, <b>Admin</b>. What do you want to do</h1>
+    <p>
+        <a href='?reboot=true' class="btn btn-danger ml-3">restart pi</a>
+        <a href="?restart=true" class="btn btn-danger ml-3">restart server</a>
+
+    </p>
+    <p>
+        <a href='?update=true' class="btn btn-warning">update server</a>
+        <a href="" class="btn btn-warning"></a>
+
+    </p>
+    <p>
+        <a href='logout.php' class="btn btn-success">Sign Out</a>
+        <a href="reset-password.php" class="btn btn-success">Reset Password</a>
+
+    </p>
+</body>
+</html>
+<?php }else{ ?>
+    <html lang="en">
 <head>
     <meta charset="UTF-8">
     <title>Welcome</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="style.css">
     <style>
         body{ font: 14px sans-serif; text-align: center; }
     </style>
@@ -27,3 +67,4 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     </p>
 </body>
 </html>
+<?php } ?>
