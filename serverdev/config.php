039d76e09343a49f01eb6c4f5a55691a93fb9a99
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-06-01 19:02:11 +1200

    Update config.php

--- serverdev/config.php
+++ serverdev/config.php
@@ -4,7 +4,7 @@ server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '41380154');
-define('DB_NAME', 'website');
+define('DB_NAME', 'Server');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
