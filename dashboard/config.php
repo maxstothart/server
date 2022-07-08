63cb7f8114a5b9eaa6ec189928c0bb48abf52514
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-07-08 14:01:16 +1200

    m

--- dashboard/config.php
+++ dashboard/config.php
@@ -2,9 +2,9 @@
 /* Database credentials. Assuming you are running MySQL
 server with default setting (user 'root' with no password) */
 define('DB_SERVER', 'localhost');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
-define('DB_NAME', 'demo');
+define('DB_USERNAME', 'server');
+define('DB_PASSWORD', '4138015462');
+define('DB_NAME', 'users');
  
 /* Attempt to connect to MySQL database */
 $link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
