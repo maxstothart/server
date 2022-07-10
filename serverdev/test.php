0be07ea5bf25d1e24709f3d268df9ab548912566
maxstothart <maxstothart@hotmail.com>
2022-07-10 17:55:44 +1200

    modified:   serverdev/test.php

--- serverdev/test.php
+++ serverdev/test.php
@@ -3,7 +3,7 @@
 // (on a system with the "whoami" executable in the path)
 $output=null;
 $retval=null;
-exec('sudo whoami', $output, $retval);
+exec('sudo ls /', $output, $retval);
 echo "Returned with status $retval and output:\n";
 print_r($output);
 ?>
