9b960ae140ee671fb520e4b6d096adf18a0b870a
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-07-14 19:31:55 +1200

    update login.php

--- dashboard/login.php
+++ dashboard/login.php
@@ -62,7 +62,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
                             $_SESSION["username"] = $username;  
-                            $_SESSION["at"] = $username;                          
+                            $_SESSION["at"] = $at;                          
                             
                             // Redirect user to welcome page
                             header("location: index.php");
