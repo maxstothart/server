8ab1ade36c3212228861fcb8e0522d838d15634e
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-07-14 23:56:49 +1200

    update

--- dashboard/messenger/read.php
+++ dashboard/messenger/read.php
@@ -1,10 +1,11 @@
-<?php require_once "config.php"; ?>
 <?php
 // Check existence of id parameter before processing further
 if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+    // Include config file
+    require_once "config.php";
     
     // Prepare a select statement
-    $sql = "SELECT * FROM messenger WHERE uto = '$username' OR ufrom = '$username'";
+    $sql = "SELECT * FROM messenger WHERE id = ?";
     
     if($stmt = mysqli_prepare($link, $sql)){
         // Bind variables to the prepared statement as parameters
