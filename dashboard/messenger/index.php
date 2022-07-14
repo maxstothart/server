b992276aa7f47f411268baffd1d02c83bbba6df7
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-07-14 22:14:38 +1200

    update

--- dashboard/messenger/index.php
+++ dashboard/messenger/index.php
@@ -1,7 +1,7 @@
 <?php
 // Initialize the session
 session_start();
-username=($_SESSION["username"]);
+$username = ($_SESSION["username"]);
  
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
