a67c2ef3abaea6acbc43e4647a121abb1393ed95
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-07-14 23:31:32 +1200

    update

--- dashboard/messenger/index.php
+++ dashboard/messenger/index.php
@@ -64,7 +64,6 @@
                                         echo "<td>" . $row['message'] . "</td>";
                                         echo "<td>";
                                             echo '<a href="read.php?id='. $row['id'] .'" class="mr-3" title="View Record" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
-                                            echo '<a href="update.php?id='. $row['id'] .'" class="mr-3" title="Update Record" data-toggle="tooltip"><span class="fa fa-pencil"></span></a>';
                                             echo '<a href="delete.php?id='. $row['id'] .'" title="Delete Record" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
                                         echo "</td>";
                                     echo "</tr>";
