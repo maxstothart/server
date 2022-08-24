bca074f79e46bbb2f0f6e00de435ffa21badc2ad
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-08-25 08:10:12 +1200

    update

--- dashboard/index.php
+++ dashboard/index.php
@@ -25,7 +25,7 @@ if (isset($_GET['reboot'])) {
 <head>
     <meta charset="UTF-8">
     <title>Admin Console</title>
-    <link rel="icon" type="image/x-icon" href="http://windmillinc.tk/logo.png">
+    <link rel="icon" type="image/x-icon" href="http://windmill-inc.com/logo.png">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         body{ font: 14px sans-serif; text-align: center; }
@@ -60,7 +60,7 @@ if (isset($_GET['reboot'])) {
 <head>
     <meta charset="UTF-8">
     <title>Dashboard</title>
-    <link rel="icon" type="image/x-icon" href="http://windmillinc.tk/logo.png">
+    <link rel="icon" type="image/x-icon" href="http://windmill-inc.com/logo.png">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         body{ font: 14px sans-serif; text-align: center; }
@@ -83,7 +83,7 @@ if (isset($_GET['reboot'])) {
 <head>
     <meta charset="UTF-8">
     <title>Dashboard</title>
-    <link rel="icon" type="image/x-icon" href="http://windmillinc.tk/logo.png">
+    <link rel="icon" type="image/x-icon" href="http://windmill-inc.com/logo.png">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <style>
         body{ font: 14px sans-serif; text-align: center; }
