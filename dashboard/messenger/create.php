27cce0d1a8b375c4c4b8633d53f7ae4441e49c6f
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-11-14 09:41:06 +1300

    update

--- dashboard/messenger/create.php
+++ dashboard/messenger/create.php
@@ -325,7 +325,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                                     if (mysqli_num_rows($result) > 0) {
                                         // output data of each row
                                         while($row = mysqli_fetch_assoc($result)) {
-                                        echo "id: " . $row["id"]. " - Name: " . $row["username"]. " " . $row["aname"]. "<br>";
+                                        echo "<option value='" . $row["username"]. "'> " . $row["aname"]. "</option>";
                                     }
                                     } else {
                                         echo "0 results";
