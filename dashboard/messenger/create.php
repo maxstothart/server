c7ac48a4f43a256fc957d460be6c968aacfc3768
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-11-14 09:30:52 +1300

    update

--- dashboard/messenger/create.php
+++ dashboard/messenger/create.php
@@ -318,6 +318,28 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             <select id="uto" name="uto">  
                                 <option value="">To</option>
                                 <option value="US">United States of America</option>
+                                <?php
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+
+$sql = "SELECT id, username, aname FROM users";
+$result = mysqli_query($conn, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        echo "id: " . $row["id"]. " - Name: " . $row["uname"]. " " . $row["aname"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+?>
                             </select>
                             <span class="invalid-feedback"><?php echo $uto_err;?></span>
                         </div>
