43f61a28b51eefc58098be3bfdc45d64937fe266
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-11-14 16:49:29 +1300

    update

--- dashboard/messenger/create.php
+++ dashboard/messenger/create.php
@@ -256,7 +256,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                 echo '<script type="text/javascript">',
                     'redirect();',
                     'function redirect() {',
-                    'window.top.location.href = "#";',
+                    'window.top.location.href = "index.php";',
                     '}',
                     '</script>';
                 exit();
