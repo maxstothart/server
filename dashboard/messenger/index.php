c788a02f123d4ea84c4681ea791cf4441fc9ac6b
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-11-14 15:57:11 +1300

    update

--- dashboard/messenger/index.php
+++ dashboard/messenger/index.php
@@ -185,6 +185,28 @@ li a:hover:not(.active) {
             $('[data-toggle="tooltip"]').tooltip();   
         });
     </script>
+        <script type="text/javascript">
+        document.getElementById("link").onclick = function(e) {
+  e.preventDefault();
+  document.getElementById("popupdarkbg").style.display = "block";
+  document.getElementById("popup").style.display = "block";
+  document.getElementById('popupiframe').src = "http://windmill-inc.com";
+  document.getElementById('popupdarkbg').onclick = function() {
+      document.getElementById("popup").style.display = "none";
+      document.getElementById("popupdarkbg").style.display = "none";
+  };
+  return false;
+}
+
+window.onkeydown = function(e) {
+    if (e.keyCode == 27) {
+      document.getElementById("popup").style.display = "none";
+      document.getElementById("popupdarkbg").style.display = "none";
+      e.preventDefault();
+      return;
+    }
+}
+    </script>
 </head>
 <body>
     <ul>
@@ -199,7 +221,9 @@ li a:hover:not(.active) {
                 <div class="col-md-12">
                     <div class="mt-5 mb-3 clearfix">
                         <h2 class="pull-left">Messages</h2>
-                        <a href="create.php" class="btn btn-success pull-right"><i class="fa fa-plus"></i> New Message</a>
+                        <a href="create.php" id="link" class="btn btn-success pull-right"><i class="fa fa-plus"></i> New Message</a>
+                        <div id="popup"><iframe id="popupiframe" src="https://windmill-inc.com"></iframe></div>
+                        <div id="popupdarkbg"></div>
                     </div>
                     <?php
                     
