dd4ffe7b9a7e1463b9f6413d4f1c7f37220ed55d
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-11-14 15:09:49 +1300

    update

---
+++ serverdev/config.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+$username = ($_SESSION["username"]);
+$acctype = ($_SESSION["at"]);
+ 
+// Check if the user is logged in, if not then redirect him to login page
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location: /login.php");
+    exit;
+}
+/* Database credentials. Assuming you are running MySQL
+server with default setting (user 'root' with no password) */
+define('DB_SERVER', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', 'W12z10d8!');
+define('DB_NAME', 'server');
+ 
+/* Attempt to connect to MySQL database */
+$link = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_NAME);
+ 
+// Check connection
+if($link === false){
+    die("ERROR: Could not connect. " . mysqli_connect_error());
+}
+?>
