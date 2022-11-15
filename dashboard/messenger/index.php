bb3e25a73e989e9b9fadf5ad5d59b0134d6d9938
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-11-15 15:24:06 +1300

    redirect

--- dashboard/messenger/index.php
+++ dashboard/messenger/index.php
@@ -50,7 +50,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             // Attempt to execute the prepared statement
             if(mysqli_stmt_execute($stmt)){
                 // Records created successfully. Redirect to landing page
-                echo '<script type="text/javascript">closepopup();</script>',
+                header('Location: '."/messenger");
                 exit();
                 //return();
             } else{
