556d482acc22e662ef488b0e0371c043e140c56a
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-11-15 17:41:29 +1300

    update

--- serverdev/index.php
+++ serverdev/index.php
@@ -54,5 +54,21 @@
     <p>this is the text inside the modal</p>
   </div>
 </div>
+<script>
+	let modalBtn = document.getElementById("modal-btn")
+let modal = document.querySelector(".modal")
+let closeBtn = document.querySelector(".close-btn")
+modalBtn.onclick = function(){
+  modal.style.display = "block"
+}
+closeBtn.onclick = function(){
+  modal.style.display = "none"
+}
+window.onclick = function(e){
+  if(e.target == modal){
+    modal.style.display = "none"
+  }
+}
+</script>
 </body>
 </html>
