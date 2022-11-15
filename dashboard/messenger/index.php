993ff45fc3c9237bdf638430c0dfb96a90ad3c33
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-11-15 17:22:43 +1300

    update

--- dashboard/messenger/index.php
+++ dashboard/messenger/index.php
@@ -214,14 +214,47 @@ li a:hover:not(.active) {
                     <div class="mt-5 mb-3 clearfix">
                         <h2 class="pull-left">Messages</h2>
                         <a href="" id="myBtn" class="btn btn-success pull-right"><i class="fa fa-plus"></i> New Message</a>
-                        <div id="myModal" class="modal">
+<!-- The Modal -->
+<div id="myModal" class="modal">
 
-                            <!-- Modal content -->
-                            <div class="modal-content">
-                                <span class="close">&times;</span>
-                                <div id="popupthing"></div>
-                            </div>
-                        </div>    
+  <!-- Modal content -->
+  <div class="modal-content">
+    <span class="close">&times;</span>
+    <p>Some text in the Modal..</p>
+  </div>
+
+</div>
+<script>
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+var btn2 = document.getElementById("myBtn2");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+btn2.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
+</script>   
                     </div>
                     <?php
                     // Attempt select query execution
@@ -272,18 +305,6 @@ li a:hover:not(.active) {
         </div>
     </div>
     <script type="text/javascript">
-        var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-var btn2 = document.getElementById("myBtn2");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
                             function showpage(b) {
                                 if (b == 1) {
                         		document.getElementById("popupthing").innerHTML = `<div class="wrapper">
