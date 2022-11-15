a35995b0597163b78e834b7b838a3e0ed9291685
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-11-15 14:28:20 +1300

    update

--- dashboard/messenger/create.php
+++ dashboard/messenger/create.php
@@ -199,7 +199,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                             <span class="invalid-feedback"><?php echo $uto_err;?></span>
                         </div>
                         <input type="submit" class="btn btn-primary" value="Submit">
-                        <a href="index.php" class="btn btn-secondary ml-2">Cancel</a>
+                        <a onClick="window.top.location.href = 'http://www.example.com'" class="btn btn-secondary ml-2">Cancel</a>
                     </form>
                 </div>
             </div>        
