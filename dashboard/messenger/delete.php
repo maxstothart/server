08ca9efe9e4203aa3242632ca9e8e07b100b0284
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-11-16 08:38:34 +1300

    update

--- dashboard/messenger/delete.php
+++ dashboard/messenger/delete.php
@@ -60,13 +60,13 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
             <div class="row">
                 <div class="col-md-12">
                     <h2 class="mt-5 mb-3">Delete Message</h2>
-                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                    <form action="/messenger/delete.php" method="post">
                         <div class="alert alert-danger">
                             <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
                             <p>Are you sure you want to delete this message?</p>
                             <p>
                                 <input type="submit" value="Yes" class="btn btn-danger">
-                                <a href="index.php" class="btn btn-secondary">No</a>
+                                <a onClick="closepopup()" class="btn btn-secondary">No</a>
                             </p>
                         </div>
                     </form>
