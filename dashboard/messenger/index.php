8493640cf2582d1770c63a672b8c989515f5ade9
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-11-15 17:02:01 +1300

    update

--- dashboard/messenger/index.php
+++ dashboard/messenger/index.php
@@ -241,8 +241,8 @@ li a:hover:not(.active) {
                                         echo "<td>" . $row['ufrom'] . "</td>";
                                         echo "<td>" . $row['message'] . "</td>";
                                         echo "<td>";
-                                            echo '<a id="readmessage" class="mr-3" title="View Message" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
-                                            echo '<a onClick="test()" title="Delete Message" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
+                                            echo '<a id="message" class="mr-3" title="View Message" data-toggle="tooltip"><span class="fa fa-eye"></span></a>';
+                                            echo '<a id="message" title="Delete Message" data-toggle="tooltip"><span class="fa fa-trash"></span></a>';
                                             //echo '<a id="addmessage" class="btn btn-success pull-right"><i class="fa fa-plus"></i> New Message</a>';
                                             //echo '<a id="delmessage" class="btn btn-success pull-right"><i class="fa fa-plus"></i> New Message</a>';
                                             echo "</td>";
