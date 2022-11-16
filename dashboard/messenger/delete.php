7ddbe7c59ffd3cae0f7c987a255384f2ac29eddc
maxstothart <99781973+maxstothart@users.noreply.github.com>
2022-11-16 15:54:20 +1300

    update

--- dashboard/messenger/delete.php
+++ dashboard/messenger/delete.php
@@ -40,21 +40,7 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
 }
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Delete Record</title>
-    <link rel="icon" type="image/x-icon" href="http://windmillinc.tk/logo.png">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        .wrapper{
-            width: 600px;
-            margin: 0 auto;
-        }
-    </style>
-</head>
-<body style="background-color: #E3E2DE">
+
     <div class="wrapper">
         <div class="container-fluid">
             <div class="row">
@@ -74,82 +60,3 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
             </div>        
         </div>
     </div>
-</body>
-</html>
-<?php }else{ ?>
-    <?php
-// Process delete operation after confirmation
-if(isset($_POST["id"]) && !empty($_POST["id"])){
-    
-    // Prepare a delete statement
-    $sql = "DELETE FROM messenger WHERE id = ?";
-    
-    if($stmt = mysqli_prepare($link, $sql)){
-        // Bind variables to the prepared statement as parameters
-        mysqli_stmt_bind_param($stmt, "i", $param_id);
-        
-        // Set parameters
-        $param_id = trim($_POST["id"]);
-        
-        // Attempt to execute the prepared statement
-        if(mysqli_stmt_execute($stmt)){
-            // Records deleted successfully. Redirect to landing page
-            header("location: index.php");
-            exit();
-        } else{
-            echo "Oops! Something went wrong. Please try again later.";
-        }
-    }
-     
-    // Close statement
-    mysqli_stmt_close($stmt);
-    
-    // Close connection
-    mysqli_close($link);
-} else{
-    // Check existence of id parameter
-    if(empty(trim($_GET["id"]))){
-        // URL doesn't contain id parameter. Redirect to error page
-        header("location: error.php");
-        exit();
-    }
-}
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Delete Record</title>
-    <link rel="icon" type="image/x-icon" href="http://windmillinc.tk/logo.png">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        .wrapper{
-            width: 600px;
-            margin: 0 auto;
-        }
-    </style>
-</head>
-<body style="background-color: #E3E2DE">
-    <div class="wrapper">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-md-12">
-                    <h2 class="mt-5 mb-3">Delete Message</h2>
-                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
-                        <div class="alert alert-danger">
-                            <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
-                            <p>Are you sure you want to delete this message?</p>
-                            <p>
-                                <input type="submit" value="Yes" class="btn btn-danger">
-                                <a href="index.php" class="btn btn-secondary">No</a>
-                            </p>
-                        </div>
-                    </form>
-                </div>
-            </div>        
-        </div>
-    </div>
-</body>
-</html>
-<?php } ?>
