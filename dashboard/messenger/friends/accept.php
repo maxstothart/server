d51749bd74dc622fcc1ec3cd121223d82a86c25a
maxstothart <99781973+maxstothart@users.noreply.github.com>
2023-01-06 10:20:02 +1300

    kjj

--- dashboard/messenger/friends/accept.php
+++ dashboard/messenger/friends/accept.php
@@ -5,7 +5,7 @@
 if(isset($_POST["id"]) && !empty($_POST["id"])){
     
     // Prepare a delete statement
-    $sql = "UPDATE 'friends' SET accepted = '2' WHERE id = ?";
+    $sql = "UPDATE `friends` SET `accepted` = 2 WHERE id = ?";
     
     if($stmt = mysqli_prepare($link, $sql)){
         // Bind variables to the prepared statement as parameters
@@ -58,11 +58,11 @@ if(isset($_POST["id"]) && !empty($_POST["id"])){
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12">
-                    <h2 class="mt-5 mb-3">Accept</h2>
+                    <h2 class="mt-5 mb-3">Delete Message</h2>
                     <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
                         <div class="alert alert-danger">
                             <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
-                            <p>Are you sure you want to acceptn this friend request?</p>
+                            <p>Are you sure you want to delete this message?</p>
                             <p>
                                 <input type="submit" value="Yes" class="btn btn-danger">
                                 <a href="index.php" class="btn btn-secondary">No</a>
