a5e7025d8883b9a14c38367c8d6a45bb5b724688
maxstothart <99781973+maxstothart@users.noreply.github.com>
2023-01-06 08:51:50 +1300

    l

--- dashboard/messenger/friends/accept.php
+++ dashboard/messenger/friends/accept.php
@@ -1,39 +1,24 @@
-<?php require_once "config.php";?>
+<?php require_once "config.php";
+?>
 <?php
-// Check existence of id parameter before processing further
-if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
-    // Include config file
-    require_once "config.php";
+// Process delete operation after confirmation
+if(isset($_POST["id"]) && !empty($_POST["id"])){
     
-    // Prepare a select statement
-    $sql = "SELECT * FROM messenger WHERE id = ?";
+    // Prepare a delete statement
+    $sql = "UPDATE 'friends' SET accepted = '2' WHERE id = ?";
     
     if($stmt = mysqli_prepare($link, $sql)){
         // Bind variables to the prepared statement as parameters
         mysqli_stmt_bind_param($stmt, "i", $param_id);
         
         // Set parameters
-        $param_id = trim($_GET["id"]);
+        $param_id = trim($_POST["id"]);
         
         // Attempt to execute the prepared statement
         if(mysqli_stmt_execute($stmt)){
-            $result = mysqli_stmt_get_result($stmt);
-    
-            if(mysqli_num_rows($result) == 1){
-                    /* Fetch result row as an associative array. Since the result set
-                    contains only one row, we don't need to use while loop */
-                    $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
-                
-                    // Retrieve individual field value
-                    $uto = $row["uto"];
-                    $message = $row["message"];
-                    $ufrom = $row["ufrom"];
-                } else{
-                // URL doesn't contain valid id parameter. Redirect to error page
-                header("location: error.php");
-                exit();
-            }
-            
+            // Records deleted successfully. Redirect to landing page
+            header("location: index.php");
+            exit();
         } else{
             echo "Oops! Something went wrong. Please try again later.";
         }
@@ -45,30 +30,48 @@ if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
     // Close connection
     mysqli_close($link);
 } else{
-    // URL doesn't contain id parameter. Redirect to error page
-    header("location: error.php");
-    exit();
+    // Check existence of id parameter
+    if(empty(trim($_GET["id"]))){
+        // URL doesn't contain id parameter. Redirect to error page
+        header("location: error.php");
+        exit();
+    }
 }
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Delete Record</title>
+    <link rel="icon" type="image/x-icon" href="http://windmillinc.tk/logo.png">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .wrapper{
+            width: 600px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body style="background-color: #E3E2DE">
     <div class="wrapper">
         <div class="container-fluid">
             <div class="row">
                 <div class="col-md-12">
-                    <h1 class="mt-5 mb-3">View Message</h1>
-                    <div class="form-group">
-                        <label>To</label>
-                        <p><b><?php echo $row["uto"]; ?></b></p>
-                    </div>
-                    <div class="form-group">
-                        <label>From</label>
-                        <p><b><?php echo $row["ufrom"]; ?></b></p>
-                    </div>
-                    <div class="form-group">
-                        <label>Message</label>
-                        <p><b><?php echo $row["message"]; ?></b></p>
-                    </div>
-                    <p><a onClick="closepopup()" class="btn btn-primary">Back</a></p>
+                    <h2 class="mt-5 mb-3">Accept</h2>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="alert alert-danger">
+                            <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>"/>
+                            <p>Are you sure you want to acceptn this friend request?</p>
+                            <p>
+                                <input type="submit" value="Yes" class="btn btn-danger">
+                                <a href="index.php" class="btn btn-secondary">No</a>
+                            </p>
+                        </div>
+                    </form>
                 </div>
             </div>        
         </div>
     </div>
+</body>
+</html>
