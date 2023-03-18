dad0799d54079cb595ae4c32b2ca3eb771287947
maxstothart <99781973+maxstothart@users.noreply.github.com>
2023-03-18 16:32:00 +1300

    Update installserver.sh

--- Backup/bin/installserver.sh
+++ Backup/bin/installserver.sh
@@ -4,3 +4,5 @@ sudo apt install apache2 -y
 dpkg -i /root/server/cloudflared.deb
 rm -r /etc/apache2
 cp -r /root/server/Backup/apache2 /etc/apache2
+cp /root/server/Backup/bin/updateserver /bin/updateserver
+chmod a+x /bin/updateserver
